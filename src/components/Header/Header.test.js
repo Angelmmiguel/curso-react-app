57177d3090497763defa2f378e027c6bee653762
Angel M <angel@laux.es>
2017-01-02 13:34:24 +0100

    Add enzyme and update Header test

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render } from 'enzyme';
 import Header from './Header';
 
 describe('Header', () => {
 
   it('Should render the header of the page', () => {
-    const header = shallow(
+    const header = render(
       <Header />
     );
 
